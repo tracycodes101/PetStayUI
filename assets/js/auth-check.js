e9d97375f5fbef94a91d6a12edc4b49ffc0294e3
tracycodes101 <tracy.kharono@georgebrown.ca>
2025-08-03 22:54:36 -0400

    changed index.js

--- assets/js/auth-check.js
+++ assets/js/auth-check.js
@@ -12,14 +12,14 @@ const currentUrl = window.location.origin + window.location.pathname;
 
 // Define Amplify Auth configuration using placeholders
 const amplifyAuthConfig = {
-  region: '{{AWS_REGION}}', // Example: 'us-east-1'
-  userPoolId: '{{COGNITO_USER_POOL_ID}}', //write your Cognito User Pool ID here
-  userPoolWebClientId: '{{COGNITO_USER_POOL_CLIENT_ID}}', //write your Cognito User Pool Client ID here
+  region: 'us-east-2', // Example: 'us-east-1'
+  userPoolId: 'us-east-2_CJrYNzRUD', //write your Cognito User Pool ID here
+  userPoolWebClientId: '1181q9f6rle3uqibsdt87dsmee', //write your Cognito User Pool Client ID here
   oauth: {
-    domain: '{{COGNITO_DOMAIN}}', //write your Cognito Domain here
+    domain: 'us-east-2cjrynzrud.auth.us-east-2.amazoncognito.com', //write your Cognito Domain here
     scope: ['email', 'openid', 'phone'], // OAuth scopes
-    redirectSignIn: '{{REDIRECT_SIGN_IN_URL}}', //write your Redirect Sign In URL here
-    redirectSignOut: '{{REDIRECT_SIGN_OUT_URL}}', //write your Redirect Sign Out URL here
+    redirectSignIn: 'https://main.d2e5ikjs7s989k.amplifyapp.com/admin-frontend/post-login.html', //write your Redirect Sign In URL here
+    redirectSignOut: 'https://main.d2e5ikjs7s989k.amplifyapp.com/index.html', //write your Redirect Sign Out URL here
     responseType: 'code', // OAuth flow to use (code for authorization code grant)
   }
 };
--- assets/js/config.js
+++ assets/js/config.js
@@ -2,9 +2,12 @@ window.PETSTAY_CONFIG = {
   AWS_REGION: "us-east-2",
   COGNITO_USER_POOL_ID: "us-east-2_CJrYNzRUD",
   COGNITO_USER_POOL_CLIENT_ID: "1181q9f6rle3uqibsdt87dsmee",
-  COGNITO_DOMAIN: "https://us-east-2cjrynzrud.auth.us-east-2.amazoncognito.com",
-  REDIRECT_SIGN_IN_URL: "{{REDIRECT_SIGN_IN_URL}}",
-  REDIRECT_SIGN_OUT_URL: "{{REDIRECT_SIGN_OUT_URL}}",
+  COGNITO_DOMAIN: "us-east-2cjrynzrud.auth.us-east-2.amazoncognito.com",
+
+  REDIRECT_SIGN_IN_URL: "https://main.d2e5ikjs7s989k.amplifyapp.com/admin-frontend/post-login.html",
+  REDIRECT_SIGN_OUT_URL: "https://main.d2e5ikjs7s989k.amplifyapp.com/index.html",
+  REDIRECT_ADMIN_SIGN_IN_URL: "https://main.d2e5ikjs7s989k.amplifyapp.com/admin-frontend/post-login.html",
+
   BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
   ROOMS_AVAILABILITY_API_URL: "{{ROOMS_AVAILABILITY_API_URL}}",
   NEW_BOOKING_API_URL: "{{NEW_BOOKING_API_URL}}",
